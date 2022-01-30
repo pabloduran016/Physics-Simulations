7ea324c0ec7e7ba0557ef994532718f1952c62ce
pabloduran016 <pdrm56@gmail.com>
2022-01-30 21:37:51 +0100

    Support for circular waves

--- waves3d/__main__.py
+++ waves3d/__main__.py
@@ -2,6 +2,8 @@
 Script to
 Written by Pablo Duran (https://github.com/pabloduran016)
 """
+import sys
+from abc import ABC, abstractmethod
 from dataclasses import dataclass
 from typing import List
 
@@ -50,21 +52,35 @@ WAVE_FRAGMENT_SHADER = load_shader('waves3d/wave.frag')
 PLANE_VERTEX_SHADER = load_shader('waves3d/plane.vert')
 PLANE_FRAGMENT_SHADER = load_shader('waves3d/plane.frag')
 
+#
+# def load_wave_vertex_shader(waves: List['Wave3d']):
+#     ind = WAVE_VERTEX_SHADER.find('wave_func')
+#     enter = WAVE_VERTEX_SHADER[ind:].find('{') + ind + 1
+#     close = WAVE_VERTEX_SHADER[enter:].find('}') + enter
+#
+#     string = " + \n".join([f"({w.amp} * cos(d * {2*pi/w.wl} - {2*pi/w.per} * t + {w.phase}))" for w in waves])
+#
+#     body = f"""
+#     float d = sqrt(pos.x*pos.x + pos.z*pos.z);
+#     float y = ({string});
+#     return vec3(pos.x, y, pos.z);
+# """
+#
+#     return WAVE_VERTEX_SHADER[:enter] + body + WAVE_VERTEX_SHADER[close:]
 
-def load_wave_vertex_shader(waves: List['Wave3d']):
-    ind = WAVE_VERTEX_SHADER.find('wave_func')
-    enter = WAVE_VERTEX_SHADER[ind:].find('{') + ind + 1
-    close = WAVE_VERTEX_SHADER[enter:].find('}') + enter
 
-    string = " + \n".join([f"({w.amp} * cos(d * {2*pi/w.wl} - {2*pi/w.per} * t + {w.phase}))" for w in waves])
+class Sprite(ABC):
+    @abstractmethod
+    def change(self, trans=None, rot=None, model=None, proj=None) -> None:
+        raise NotImplementedError
 
-    body = f"""
-    float d = sqrt(pos.x*pos.x + pos.z*pos.z);
-    float y = ({string});
-    return vec3(pos.x, y, pos.z);
-"""
+    @abstractmethod
+    def draw(self) -> None:
+        raise NotImplementedError
 
-    return WAVE_VERTEX_SHADER[:enter] + body + WAVE_VERTEX_SHADER[close:]
+    @abstractmethod
+    def update(self, dt: float) -> None:
+        raise NotImplementedError
 
 @dataclass
 class Wave3d:
@@ -74,20 +90,40 @@ class Wave3d:
     phase: float
 
 
-class Grid:
-    def __init__(self, size: float, n: int, waves: List[Wave3d]):
+WAVES_CAP=16
+
+
+class Grid(Sprite):
+    def __init__(self, size: float, n: int, pos: Tuple[float, float, float], waves: List[Wave3d]):
         self.size = size
         self.n = n
         self.cell_size = size / n
+        self.pos = pos
 
-        v_shader = load_wave_vertex_shader(waves)
-        self.grid = gloo.Program(v_shader, WAVE_FRAGMENT_SHADER)
+        if len(waves) > WAVES_CAP:
+            print(f'Number of waves (len(waves)) is bigger than waves capacity ({WAVES_CAP})', file=sys.stderr)
+            exit(1)
 
+        self.grid = gloo.Program(WAVE_VERTEX_SHADER, WAVE_FRAGMENT_SHADER)
+
+        n_waves = len(waves)
         self.grid['size'] = size
+        self.grid['n_waves'] = n_waves
 
-        # self.grid['height'] = h
+        self.grid['amps'] = np.zeros(16, dtype=np.float32)
+        self.grid['amps'][:n_waves] = np.array([w.amp for w in waves])
+
+        self.grid['ks'] = np.zeros(16, dtype=np.float32)
+        self.grid['ks'][:n_waves] = np.array([(2*pi/w.wl) for w in waves])
+
+        self.grid['a_freqs'] = np.zeros(16, dtype=np.float32)
+        self.grid['a_freqs'][:n_waves] = np.array([2*pi/w.per for w in waves])
+
+        self.grid['phases'] = np.zeros(16, dtype=np.float32)
+        self.grid['phases'][:n_waves] = np.array([w.phase for w in waves])
 
         vertices, edges, triangles = self.gen_vertices(n, size)
+        vertices['position'] += pos
 
         self.vertices = vertices.view(gloo.VertexBuffer)
         self.triangles = triangles.view(gloo.IndexBuffer)
@@ -98,13 +134,15 @@ class Grid:
     def gen_vertices(self, n: int, cell_s: float) -> Tuple[np.ndarray, ...]:
         raise NotImplementedError
 
-    def change(self, model=None, trans=None, rot=None) -> None:
+    def change(self, model=None, trans=None, rot=None, proj=None) -> None:
         if trans is not None:
             self.grid['translation'] = trans
         if model is not None:
             self.grid['model'] = model
         if rot is not None:
             self.grid['rotation'] = rot
+        if proj is not None:
+            self.grid['projection'] = proj
 
     def update(self, dt: float):
         self.grid['t'] += dt*1000
@@ -117,14 +155,22 @@ class Grid:
         gl.glEnable(gl.GL_POLYGON_OFFSET_FILL)
         self.grid['color'] = gl_color(CELESTE)
         self.grid.draw(gl.GL_TRIANGLES, self.triangles)
-
-        # Outlined
-        gl.glDisable(gl.GL_POLYGON_OFFSET_FILL)
-        gl.glEnable(gl.GL_BLEND)
-        gl.glDepthMask(gl.GL_FALSE)
-        self.grid['color'] = gl_color(BLACK)
-        self.grid.draw(gl.GL_LINES, self.edges)
-        gl.glDepthMask(gl.GL_TRUE)
+        #
+        # # Outlined
+        # gl.glDisable(gl.GL_POLYGON_OFFSET_FILL)
+        # gl.glEnable(gl.GL_BLEND)
+        # gl.glDepthMask(gl.GL_FALSE)
+        # self.grid['color'] = gl_color(BLACK)
+        # self.grid.draw(gl.GL_LINES, self.edges)
+        # gl.glDepthMask(gl.GL_TRUE)
+        #
+        # # Points
+        # gl.glDisable(gl.GL_POLYGON_OFFSET_FILL)
+        # gl.glEnable(gl.GL_BLEND)
+        # gl.glDepthMask(gl.GL_FALSE)
+        # self.grid['color'] = gl_color(BLACK)
+        # self.grid.draw(gl.GL_POINTS)
+        # gl.glDepthMask(gl.GL_TRUE)
 
 
 class SquaredGrid(Grid):
@@ -152,39 +198,57 @@ class SquaredGrid(Grid):
         return vertices.reshape((n*n)), edges.reshape((n-1)*n*2*2), triangles.reshape(((n-1)**2)*2*3)
 
 
+N_ANGLES = 30
 class CircularGrid(Grid):
     def gen_vertices(self, n: int, size: float) -> Tuple[np.ndarray, ...]:
-        cell_s = size / n
         r = size / 2
-        s = r/ n
-        # TODO: Use the height to create a water cube
-        vertices = np.zeros((n, n), [('position', np.float32, 3)])
-        triangles = np.zeros((n - 1, n - 1, 2, 3), dtype=np.uint32)
-        edges = np.zeros((n - 1, n, 2, 2), dtype=np.uint32)
-        for i in range(n):
-            for k in range(n):
-                x = (i - (n - 1) / 2) * cell_s
-                z = (k - (n - 1) / 2) * cell_s
-                d = np.sqrt(x**2 + z**2)
-                if d > r:
-                    continue
-                vertices[i, k]['position'] = [x, 0, z]
-                if i != n - 1 and k != n - 1:
-                    triangles[i, k, 0, :] = [i * n + k, i * n + k + 1, (i + 1) * n + k]
-                    triangles[i, k, 1, :] = [i * n + k + 1, (i + 1) * n + k, (i + 1) * n + k + 1]
+        n = n + 1 if n % 2 == 0 else n
+        d_angle = 360 / (N_ANGLES)
+        vertices = np.zeros(N_ANGLES * n, [('position', np.float32, 3)])
+        edges = np.zeros((N_ANGLES, n, 2, 2), np.uint32)
+        triangles = np.zeros((N_ANGLES, (n - 1), 2, 3), np.uint32)
+        for a in range(N_ANGLES):
+            for p in range(n):
+                angle = a * d_angle/2
+                point = (p - (n//2)) / ((n - 1) // 2)
+                x = point * np.cos(angle * pi / 180)
+                z = point * np.sin(angle * pi / 180)
+                vertices[a*n + p]['position'] = [r*x, 0, r*z]
 
-                if i != n - 1 and k != n - 1:
-                    edges[i, k, 0, :] = [i * n + k, i * n + k + 1]
-                    edges[i, k, 1, :] = [i * n + k, (i + 1) * n + k]
+                if p < n - 1:
+                    if a < N_ANGLES - 1:
+                        edges[a, p, 0] = (a*n + p, a*n + p + 1)
+                        edges[a, p, 1] = (a*n + p, (a+1)*n + p)
+                        if p == (n - 1)//2 - 1: # center triangles
+                            triangles[a, p, 0] = a*n + p, a*n + p + 1, (a + 1)*n + p
+                        elif p == (n - 1)//2: # center triangles
+                            triangles[a, p, 0] = a*n + p, a*n + p + 1, (a + 1)*n + p + 1
+                        elif point > 0:
+                            triangles[a, p - 1, 0] = a*n + p, a*n + p + 1, (a + 1)*n + p + 1
+                            triangles[a, p - 1, 1] = (a + 1)*n + p, a*n + p, (a + 1)*n + p + 1
+                        elif point < 0:
+                            triangles[a, p, 0] = a*n + p, a*n + p + 1, (a + 1)*n + p + 1
+                            triangles[a, p, 1] = (a + 1)*n + p, a*n + p, (a + 1)*n + p + 1
+                    else:
+                        edges[a, p, 0] = (a*n + p, a*n + p + 1)
+                        edges[a, p, 1] = (0 + (n - 1 - p), a*n + p)
+                        if p == (n - 1)//2 - 1: # center triangles
+                            triangles[a, p, 0] = a*n + p, a*n + p + 1, 0*n + (n - 1 - p)
+                        elif p == (n - 1)//2: # center triangles
+                            triangles[a, p, 0] = a*n + p, a*n + n - p, 0*n + n - p - 2
+                        elif point > 0:
+                            triangles[a, p - 1, 0] = a*n + p, a*n + p + 1, 0*n + n - p - 1
+                            triangles[a, p - 1, 1] = a*n + p + 1, 0*n + n - p - 2, 0*n + n - p - 1
+                        elif point < 0:
+                            triangles[a, p, 0] = a*n + p, a*n + p + 1, 0*n + n - p - 1
+                            triangles[a, p, 1] = a*n + p + 1, 0*n + n - p - 2, 0*n + n - p - 1
 
-                elif k == n - 1 and i != n - 1:
-                    edges[i, k, 0, :] = [k * n + i, k * n + i + 1]
-                    edges[i, k, 1, :] = [i * n + k, (i + 1) * n + k]
-
-        return vertices.reshape((n*n)), edges.reshape((n-1)*n*2*2), triangles.reshape(((n-1)**2)*2*3)
+        edges = edges.reshape(N_ANGLES*(n)*2*2)
+        triangles = triangles.reshape(N_ANGLES*(n -1)*2*3)
+        return vertices.reshape(N_ANGLES*n), edges, triangles
 
 
-class Plane:
+class Plane(Sprite):
     def __init__(self, y: float, color: GLColor):
         self.plane = gloo.Program(PLANE_VERTEX_SHADER, PLANE_FRAGMENT_SHADER)
 
@@ -200,14 +264,17 @@ class Plane:
         self.plane['zfar'] = ZFAR
         self.plane.bind(self.vertices)
 
-    def change(self, trans=None, rot=None) -> None:
+    def change(self, trans=None, rot=None, model=None, proj=None) -> None:
         if trans is not None:
             self.plane['translation'] = trans
         if rot is not None:
             self.plane['rotation'] = rot
+        if proj is not None:
+            self.plane['projection'] = proj
+        if model is not None:
+            pass
 
     def update(self, dt: float):
-        self.plane['t'] += dt*1000
         pass
 
     def draw(self):
@@ -222,8 +289,9 @@ class Plane:
 PLANE_COLOR = gl_color(GREY)
 PLANE_Y = -15
 
-GRID_SIZE = 50
-GRID_N = 800
+GRID_SIZE = 100
+GRID_N = 200
+GRID_POS = 0, 0, 0
 WCUBE_HEIGHT = 10
 
 WAVE_AMP = 1.5
@@ -232,12 +300,14 @@ WAVE_WL = 3.5
 WAVE_PHASE = 0
 
 FOV = 60  # field of view
-ZNEAR, ZFAR = .1, 150
+ZNEAR, ZFAR = .1, 1000
 # CAM_INITIAL_POSITION = 0, -30, -20
 # CAM_INITIAL_ANGLE = 60
 #
 # VELOCITY = .1
 # ROT_VEL = .1
+
+MAX_CHR = 0x110000
 
 
 class Simulation3D:
@@ -256,14 +326,21 @@ class Simulation3D:
         self.translation = np.eye(4, dtype=np.float32)
         self.rotation = np.eye(4, dtype=np.float32)
 
-        self.grid = SquaredGrid(GRID_SIZE, GRID_N, [
-            Wave3d(1, 4e3, 3.5, WAVE_PHASE),
-            Wave3d(1.5, 1e3, 8, WAVE_PHASE),
-            Wave3d(8, 5e3, 20, WAVE_PHASE),
-            Wave3d(1, .5e3, 2, WAVE_PHASE),
-        ])
-        # self.plane = Plane(PLANE_Y, PLANE_COLOR)
-        self.plane = Plane(PLANE_Y, PLANE_COLOR)
+        self.sprites: List[Sprite] = [
+            # SquaredGrid(GRID_SIZE, GRID_N, (GRID_POS[0], GRID_POS[1] + 10, GRID_POS[2]), [
+            #     Wave3d(1, 4e3, 3.5, WAVE_PHASE),
+            #     # Wave3d(1.5, 1e3, 8, WAVE_PHASE),
+            #     # Wave3d(8, 5e3, 20, WAVE_PHASE),
+            #     # Wave3d(1, .5e3, 2, WAVE_PHASE),
+            # ]),
+            CircularGrid(GRID_SIZE, GRID_N, GRID_POS, [
+                Wave3d(1, 4e3, 3.5, WAVE_PHASE),
+                Wave3d(1.5, 1e3, 8, WAVE_PHASE),
+                Wave3d(8, 5e3, 20, WAVE_PHASE),
+                Wave3d(1, .5e3, 2, WAVE_PHASE),
+            ]),
+            Plane(PLANE_Y, PLANE_COLOR),
+        ]
         self.reset()
 
     def reset(self):
@@ -275,37 +352,31 @@ class Simulation3D:
         glm.translate(translation, 0, 0, -GRID_SIZE*1.5)
         self.translation = translation
         self.rotation = rotation
-        self.grid.change(model=model, trans=translation, rot=rotation)
-        self.plane.change(trans=translation, rot=rotation)
+        for obj in self.sprites:
+            obj.change(model=model, trans=translation, rot=rotation)
 
-    def translatex(self, dist):
-        glm.translate(self.translation, dist, 0, 0)
-        self.grid.change(trans=self.translation)
-        self.plane.change(trans=self.translation)
+    def translate(self, x, y, z):
+        glm.translate(self.translation, x, y, z)
+        for obj in self.sprites:
+            obj.change(trans=self.translation)
 
-    def translatey(self, dist):
-        glm.translate(self.translation, 0, dist, 0)
-        self.grid.change(trans=self.translation)
-        self.plane.change(trans=self.translation)
+    def rotate(self, angle, x, y, z):
+        glm.rotate(self.rotation, angle, x, y, z)
+        for obj in self.sprites:
+            obj.change(rot=self.rotation)
 
-    def translatez(self, dist):
-        glm.translate(self.translation, 0, 0, dist)
-        self.grid.change(trans=self.translation)
-        self.plane.change(trans=self.translation)
+    def translatex(self, dist): self.translate(dist, 0, 0)
 
-    def rotatex(self, angle):
-        glm.rotate(self.rotation, angle, 1, 0, 0)
-        self.grid.change(rot=self.rotation)
-        self.plane.change(rot=self.rotation)
+    def translatey(self, dist): self.translate(0, dist, 0)
 
-    def rotatey(self, angle):
-        glm.rotate(self.rotation, angle, 0, 1 , 0)
-        self.grid.change(rot=self.rotation)
-        self.plane.change(rot=self.rotation)
+    def translatez(self, dist): self.translate(0, 0, dist)
 
+    def rotatex(self, angle): self.rotate(angle, 1, 0, 0)
+
+    def rotatey(self, angle): self.rotate(angle, 0, 1, 0)
 
     def on_key_press(self, symbol, modifiers):
-        if 0 < symbol < 0x110000:
+        if 0 < symbol < MAX_CHR:
             if chr(symbol).lower() == 'r':
                 self.reset()
         if modifiers & (key.LCTRL | key.RCTRL):
@@ -343,21 +414,22 @@ class Simulation3D:
     def on_resize(self, width, height):
         ratio = width / height
         proj = glm.perspective(FOV, ratio, ZNEAR, ZFAR)
-        self.grid.grid['projection'] = proj
-        self.plane.plane['projection'] = proj
+        for obj in self.sprites:
+            obj.change(proj=proj)
         # self.grid.grid['projection'] = glm.ortho(-2, 2, -2, 2, .1, 100)
 
     def start(self) -> None:
         app.run(framerate=FPS)
 
     def update(self, dt: float):
-        self.grid.update(dt)
+        for obj in self.sprites:
+            obj.update(dt)
 
     def on_draw(self, dt: float):
         self.update(dt)
         self.window.clear()
-        self.grid.draw()
-        self.plane.draw()
+        for obj in self.sprites:
+            obj.draw()
 
 
 if __name__ == '__main__':
--- waves3d/wave.vert
+++ waves3d/wave.vert
@@ -5,25 +5,42 @@ uniform mat4   projection;    // Projection matrix
 attribute vec3 position;      // Vertex position
 uniform float t;
 uniform float size;
-uniform float amp;
-uniform float k;
-uniform float a_freq;
-uniform float phase;
-uniform vec4 color;
+uniform int n_waves;
+
+uniform mat4 amps;
+uniform mat4 ks;
+uniform mat4 a_freqs;
+uniform mat4 phases;
+
+//#define WAVES_CAP 25
+//uniform float amps[WAVES_CAP];
+//uniform float ks[WAVES_CAP];
+//uniform float a_freqs[WAVES_CAP];
+//uniform float phases[WAVES_CAP];
+
 varying vec4 v_color;
+uniform vec4 color;
+
+
+#define amp 1. 
+#define k 1.7951958
+#define a_freq 0.0015708
+#define phase 0.
 
 vec3 wave_func(vec3 pos)
 {
-    // filled in the porgram preprocessing
-      float d = sqrt(pos.x*pos.x + pos.z*pos.z);
-      float y = amp * cos(d * k - a_freq * t + phase);
-      return vec3(pos.x, y, pos.z);
-    // ------------------------------------------
+    float d = sqrt(pos.x*pos.x + pos.z*pos.z);
+    for (int i = 0; i < 4; i++) {
+        for (int j = 0; j < 4; j++) {
+            pos.y += amps[i][j] * cos(d * ks[i][j] - a_freqs[i][j] * t + phases[i][j]);
+        }
+    }
     return pos;
 }
 
 void main()
 {
+    gl_PointSize = 10;
     vec4 pos = projection * translation * rotation * model * vec4(wave_func(position), 1.0);
     gl_Position = pos;
     v_color = color / pos.w * size;
